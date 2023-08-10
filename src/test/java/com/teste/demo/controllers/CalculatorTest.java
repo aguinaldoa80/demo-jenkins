de1b061e3f6311bdcdcd71ffc9a777a8ab094af9
Aguinaldo Moreira <aguinaldo.moreira@senior.com.br>
2023-08-10 14:16:32 -0300

    remove unusable imports

--- src/test/java/com/teste/demo/controllers/CalculatorTest.java
+++ src/test/java/com/teste/demo/controllers/CalculatorTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 public class CalculatorTest {
